5156c66bb5ac5f827756912eff604eda0bf56ad1
Shterion <shterion.yanev@yahoo.com>
2018-08-28 14:31:54 +0200

    Create test suites for transformation testing

--- spec/transform.spec.js
+++ spec/transform.spec.js
@@ -1,48 +1,70 @@
-// /* eslint-disable arrow-body-style */
-//
-// const expect = require('chai').expect;
-// const transform = require('../lib/actions/transform');
-// const eioUtils = require('elasticio-node').messages;
-//
-// describe('Transformation test', () => {
-//   it('should handle simple transforms', () => {
-//     return transform.process(eioUtils.newMessageWithBody({
-//       first: 'Renat',
-//       last: 'Zubairov'
-//     }), {
-//       expression: `{ "fullName": first & " " & last }`
-//     }).then(result => {
-//       expect(result.body).to.deep.equal({
-//         fullName: 'Renat Zubairov'
-//       });
-//     });
-//   });
-//
-//   it('should not produce an empty message if transformation returns undefined', () => {
-//     return transform.process(eioUtils.newMessageWithBody({
-//       first: 'Renat',
-//       last: 'Zubairov'
-//     }), {
-//       expression: `$[foo=2].({ "foo": boom })`
-//     }).then(result => {
-//       expect(result).to.be.an('undefined');
-//     });
-//   });
-//
-//   it('should handle passthough properly', () => {
-//     const msg = eioUtils.newMessageWithBody({
-//       first: 'Renat',
-//       last: 'Zubairov'
-//     });
-//     msg.passthrough = {
-//       ps: 'psworks'
-//     };
-//     return transform.process(msg, {
-//       expression: `{ "fullName": first & " " & elasticio.ps}`
-//     }).then(result => {
-//       expect(result.body).to.deep.equal({
-//         fullName: 'Renat psworks'
-//       });
-//     });
-//   });
-// });
+/* eslint-disable arrow-body-style */
+/* eslint-disable no-unused-expressions */
+
+const {expect} = require('chai');
+const {messages} = require('elasticio-node');
+const {personFromOih, personToOih} = require('./seed/person');
+const transformPersonFromOih = require('../lib/actions/transformPersonFromOih');
+const transformPersonToOih = require('../lib/actions/transformPersonToOih');
+
+describe('Transformation test', () => {
+  it('should handle simple person tranformation in direction from OIH', () => {
+    const exp = personFromOih();
+    return transformPersonFromOih.process(messages.newMessageWithBody(exp))
+      .then(result => {
+        expect(result.body).to.be.an('object');
+        expect(result.body).to.deep.include({
+          rowid: 12121,
+          name: 'Doe',
+          firstname: 'John',
+          position: 'Sales manager',
+          private_street: 'Hohestr',
+          private_street_number: '3',
+          private_zip_code: '50667',
+          private_town: 'Cologne',
+          private_country: 'Germany',
+          email: 'jon@doe.com',
+          phone: '123456789',
+          mobile_phone: '98326307',
+          xing_url: 'xing.de/yourUsername',
+          last_update: 36542364
+        });
+      });
+  });
+
+  it('should not produce an empty message if transformation returns undefined', () => {
+    return transformPersonFromOih.process(messages.newMessageWithBody({}))
+      .then(result => {
+        expect(result).to.be.undefined;
+      });
+  });
+
+  it('should handle simple person tranformation in direction to OIH', () => {
+    const exp = personToOih();
+    return transformPersonToOih.process(messages.newMessageWithBody(exp))
+      .then(result => {
+        expect(result.body).to.be.an('object');
+        expect(result.body).to.deep.include({
+          firstName: 'Mark',
+          lastName: 'Smith',
+          jobTitle: 'Marketing Manager'
+        });
+        expect(result.body.test).to.be.undefined;
+        expect(result.body.oihApplicationRecords[0]).to.deep.include({
+          recordUid: 98123
+        });
+        expect(result.body.addresses).to.be.an('array');
+        expect(result.body.addresses[0]).to.deep.include({
+          street: 'Main Str.',
+          streetNumber: 120
+        });
+      });
+  });
+
+  it('should not produce an empty message if transformation returns undefined', () => {
+    return transformPersonToOih.process(messages.newMessageWithBody({}))
+      .then(result => {
+        expect(result).to.be.undefined;
+      });
+  });
+});
