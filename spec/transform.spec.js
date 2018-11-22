c271273b38f8b02f38b51d90512e72af9b4cbeb6
Shterion <shterion.yanev@yahoo.com>
2018-11-22 21:49:26 +0100

    Debugging

--- spec/transform.spec.js
+++ spec/transform.spec.js
@@ -1,71 +1,70 @@
-// /* eslint-disable arrow-body-style */
-// /* eslint-disable no-unused-expressions */
-//
-// const {expect} = require('chai');
-// const {messages} = require('elasticio-node');
-// const {personFromOih, personToOih} = require('./seed/person');
-// const transformPersonFromOih = require('../lib/actions/transformPersonFromOih');
-// const transformPersonToOih = require('../lib/actions/transformPersonToOih');
-//
-// describe('Transformation test', () => {
-//   it('should handle simple person tranformation in direction from OIH', () => {
-//     const exp = personFromOih();
-//     return transformPersonFromOih.process(messages.newMessageWithBody(exp))
-//       .then(result => {
-//         expect(result.body).to.be.an('object');
-//         expect(result.body).to.deep.include({
-//           rowid: exp.oihApplicationRecords[1].recordUid,
-//           name: exp.lastName,
-//           firstname: exp.firstName,
-//           position: exp.jobTitle,
-//           private_street: exp.addresses[0].street,
-//           private_street_number: exp.addresses[0].streetNumber,
-//           private_zip_code: exp.addresses[0].zipCode,
-//           private_town: exp.addresses[0].city,
-//           private_country: exp.addresses[0].country,
-//           email: exp.contactData[3].value,
-//           phone: exp.contactData[0].value,
-//           mobile_phone: exp.contactData[5].value,
-//           xing_url: exp.contactData[4].value,
-//           last_update: exp.oihApplicationRecords[0].lastModified
-//         });
-//       });
-//   });
-//
-//   it('should not produce an empty message if transformation returns undefined', () => {
-//     return transformPersonFromOih.process(messages.newMessageWithBody({}))
-//       .then(result => {
-//         expect(result).to.be.undefined;
-//       });
-//   });
-//
-//   it('should handle simple person tranformation in direction to OIH', () => {
-//     const exp = personToOih();
-//     const street = exp.private_street.split(' ');
-//     return transformPersonToOih.process(messages.newMessageWithBody(exp))
-//       .then(result => {
-//         expect(result.body).to.be.an('object');
-//         expect(result.body).to.deep.include({
-//           firstName: exp.firstname,
-//           lastName: exp.name,
-//           jobTitle: exp.position
-//         });
-//         expect(result.body.test).to.be.undefined;
-//         expect(result.body.oihApplicationRecords[0]).to.deep.include({
-//           recordUid: exp.rowid
-//         });
-//         expect(result.body.addresses).to.be.an('array');
-//         expect(result.body.addresses[0]).to.deep.include({
-//           street: `${street[0]} ${street[1]}`,
-//           streetNumber: parseInt(street[2], 10)
-//         });
-//       });
-//   });
-//
-//   it('should not produce an empty message if transformation returns undefined', () => {
-//     return transformPersonToOih.process(messages.newMessageWithBody({}))
-//       .then(result => {
-//         expect(result).to.be.undefined;
-//       });
-//   });
-// });
+/* eslint-disable arrow-body-style */
+/* eslint-disable no-unused-expressions */
+
+const {expect} = require('chai');
+const {messages} = require('elasticio-node');
+const {personFromOih, personToOih} = require('./seed/person');
+const transformPersonFromOih = require('../lib/actions/transformPersonFromOih');
+const transformPersonToOih = require('../lib/actions/transformPersonToOih');
+
+describe('Transformation test', () => {
+  it('should handle simple person tranformation in direction from OIH', () => {
+    const exp = personFromOih();
+    return transformPersonFromOih.process(messages.newMessageWithBody(exp))
+      .then(result => {
+        expect(result.body).to.be.an('object');
+        expect(result.body).to.deep.include({
+          rowid: exp.oihApplicationRecords[1].recordUid,
+          name: exp.lastName,
+          firstname: exp.firstName,
+          position: exp.jobTitle,
+          private_street: exp.addresses[0].street,
+          private_street_number: exp.addresses[0].streetNumber,
+          private_zip_code: exp.addresses[0].zipCode,
+          private_town: exp.addresses[0].city,
+          private_country: exp.addresses[0].country,
+          email: exp.contactData[3].value,
+          phone: exp.contactData[0].value,
+          mobile_phone: exp.contactData[5].value,
+          xing_url: exp.contactData[4].value,
+          last_update: exp.oihApplicationRecords[0].lastModified
+        });
+      });
+  });
+
+  it('should not produce an empty message if transformation returns undefined', () => {
+    return transformPersonFromOih.process(messages.newMessageWithBody({}))
+      .then(result => {
+        expect(result).to.be.undefined;
+      });
+  });
+
+  it('should handle simple person tranformation in direction to OIH', () => {
+    const exp = personToOih();
+    return transformPersonToOih.process(messages.newMessageWithBody(exp))
+      .then(result => {
+        expect(result.body).to.be.an('object');
+        expect(result.body).to.deep.include({
+          firstName: exp.firstname,
+          lastName: exp.name,
+          jobTitle: exp.position
+        });
+        expect(result.body.test).to.be.undefined;
+        expect(result.body.oihApplicationRecords[0]).to.deep.include({
+          recordUid: exp.rowid
+        });
+        expect(result.body.addresses).to.be.an('array');
+        expect(result.body.addresses[0]).to.deep.include({
+          street: 'Main Str.',
+          streetNumber: '120'
+        });
+      });
+  });
+
+  it('should not produce an empty message if transformation returns undefined', () => {
+    return transformPersonToOih.process(messages.newMessageWithBody({}))
+      .then(result => {
+        expect(result).to.be.undefined;
+      });
+  });
+});
