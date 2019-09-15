f4d16f7f25b4957103f185795f7efef593ce0164
Shterion <shterion.yanev@yahoo.com>
2019-09-15 11:19:14 +0200

    Linting

--- spec/transform.spec.js
+++ spec/transform.spec.js
@@ -1,9 +1,9 @@
 /* eslint-disable arrow-body-style */
 /* eslint-disable no-unused-expressions */
 
-const {expect} = require('chai');
-const {messages} = require('elasticio-node');
-const {personFromOih, personToOih} = require('./seed/person');
+const { expect } = require('chai');
+const { messages } = require('elasticio-node');
+const { personFromOih, personToOih } = require('./seed/person');
 const transformPersonFromOih = require('../lib/actions/transformPersonFromOih');
 const transformPersonToOih = require('../lib/actions/transformPersonToOih');
 
@@ -11,32 +11,32 @@ describe('Transformation test', () => {
   it('should handle simple person tranformation in direction from OIH', () => {
     const exp = personFromOih();
     return transformPersonFromOih.process(messages.newMessageWithBody(exp))
-      .then(result => {
+      .then((result) => {
         expect(result.body).to.be.an('object');
         expect(result.body.data.firstname).to.be.equal('John');
         expect(result.body.data.name).to.be.equal('Doe');
         // expect(result.body).to.deep.include({
-          // rowid: '98765',
-          // name: 'Doe',
-          // firstname: 'John'
-          // position: 'Sales manager',
-          // private_street: 'Hohestr',
-          // private_street_number: '3',
-          // private_zip_code: '50667',
-          // private_town: 'Cologne',
-          // private_country: 'Germany',
-          // email: 'jon@doe.com',
-          // phone: '123456789',
-          // mobile_phone: '98326307',
-          // xing_url: 'xing.de/yourUsername',
-          // last_update: '2018-01-02'
+        // rowid: '98765',
+        // name: 'Doe',
+        // firstname: 'John'
+        // position: 'Sales manager',
+        // private_street: 'Hohestr',
+        // private_street_number: '3',
+        // private_zip_code: '50667',
+        // private_town: 'Cologne',
+        // private_country: 'Germany',
+        // email: 'jon@doe.com',
+        // phone: '123456789',
+        // mobile_phone: '98326307',
+        // xing_url: 'xing.de/yourUsername',
+        // last_update: '2018-01-02'
         // });
       });
   });
 
   it('should produce an empty message if transformation returns undefined', () => {
     return transformPersonFromOih.process(messages.newMessageWithBody({}))
-      .then(result => {
+      .then((result) => {
         expect(result).to.be.undefined;
       });
   });
@@ -44,7 +44,7 @@ describe('Transformation test', () => {
   it('should handle simple person tranformation in direction to OIH', () => {
     const exp = personToOih();
     return transformPersonToOih.process(messages.newMessageWithBody(exp))
-      .then(result => {
+      .then((result) => {
         expect(result.body).to.be.an('object');
         expect(result.body.data.firstName).to.be.equal('Mark');
         expect(result.body.data.lastName).to.be.equal('Smith');
@@ -67,7 +67,7 @@ describe('Transformation test', () => {
 
   it('should produce an empty message if transformation returns undefined', () => {
     return transformPersonToOih.process(messages.newMessageWithBody({}))
-      .then(result => {
+      .then((result) => {
         expect(result).to.be.undefined;
       });
   });
